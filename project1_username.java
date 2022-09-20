54dbbc13bed26ce340f780b70fb9cc9d26aa2ee6
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-09-20 20:34:55 -0300

    Start coding project - username

---
+++ project1_username.java
@@ -0,0 +1,18 @@
+import java.util.*
+import java.io.*
+
+public class Main{
+
+	public static void main(String[] args) {
+
+		String rules;
+
+		if (rules == true){
+			System.out.println("This username complies with the rules");
+		} else{
+			System.out.println("This username is not allowed");
+		}	
+		
+	}
+}
+
