483e4c3efca44edcf7d25445537589cac2b68b49
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-09-21 19:30:24 -0300

    Creating Strings
    Variable username defined;
    Strings with messages shown on screen were created;
    Variable username has a value assigned after it's creation by user.

--- project1_username.java
+++ project1_username.java
@@ -5,9 +5,20 @@ public class Main{
 
 	public static void main(String[] args) {
 
+		String username;
 		boolean rules;
 		Scanner scanner = new Scanner (System.in);
 
+		System.out.println("Create a username following these simple rules: ");
+
+		//Maybe I should do a table to show the rules (?)
+		System.out.println("1- The username is between 4 and 25 characters.");
+		System.out.println("2- It must start with a lower case letter.");
+		System.out.println("3- It can only contain letters, numbers, and the underscore character.");
+		System.out.println("4- It cannot start with an underscore character.");
+
+		System.out.println("Choose your username: ");
+		username = scanner.next();
 
 		if (rules == true){
 			System.out.print("This username complies with the rules");
