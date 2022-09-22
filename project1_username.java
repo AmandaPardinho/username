bff89cd1c698249514459b1488029b1f1d6d28cf
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-09-22 00:31:11 -0300

    Commenting first IF and creating other IF with username.lenght()

--- project1_username.java
+++ project1_username.java
@@ -1,14 +1,43 @@
-import java.util.*
-import java.io.*
+import java.util.*;
+import java.io.*;
 
 public class Main{
 
 	public static void main(String[] args) {
 
+		String username;
 		boolean rules;
+		char letter;
+		char number;
+		char underscore;
 		Scanner scanner = new Scanner (System.in);
 
 
+		System.out.println("Create a username following these simple rules: ");
+
+		//Maybe I should do a table to show the rules (?)
+		System.out.println("1- The username is between 4 and 25 characters.");
+		System.out.println("2- It must start with a letter.");
+		System.out.println("3- It can only contain letters, numbers, and the underscore character.");
+		System.out.println("4- It cannot start with an underscore character.");
+
+		System.out.println("Choose your username: ");
+		username = scanner.next();
+
+		if(username.length() < 4 || username.length() > 25){
+			rules = false;
+		}else{
+			rules = true;
+		}
+
+		/*if(username == charAt(0)){
+			rules = true;
+		} else{
+			if(number = username.charAt(0) || underscore = username.charAt(0)){
+				rules = false;
+			}
+		}*/
+		
 		if (rules == true){
 			System.out.print("This username complies with the rules");
 		} else{
