9b5ac86621e203242c2753e3a82a01090bd6979d
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-09-22 19:46:33 -0300

    Corrections
    -Change the type of variables "length", "startLetter", "haveNumber" and
    "underscore";
    -Username length determined;
    -Fix rule number 4;
    -Fix package.

--- project1_username.java
+++ project1_username.java
@@ -1,3 +1,5 @@
+package projeto1_username;
+
 import java.util.*;
 import java.io.*;
 
@@ -7,9 +9,11 @@ public class Main{
 
 		String username;
 		boolean rules;
-		char letter;
-		char number;
-		char underscore;
+		boolean length;
+		boolean startLetter;
+		boolean haveNumber;
+		boolean underscore;
+		
 		Scanner scanner = new Scanner (System.in);
 
 
@@ -19,30 +23,42 @@ public class Main{
 		System.out.println("1- The username is between 4 and 25 characters.");
 		System.out.println("2- It must start with a letter.");
 		System.out.println("3- It can only contain letters, numbers, and the underscore character.");
-		System.out.println("4- It cannot start with an underscore character.");
+		System.out.println("4- It cannot finish with an underscore character.");
 
 		System.out.println("Choose your username: ");
 		username = scanner.next();
 
 		if(username.length() < 4 || username.length() > 25){
-			rules = false;
+			length = false;
 		}else{
-			rules = true;
+			length = true;
 		}
 
-		if(username.substring(0).matches("[A-Z]")){
-			rules = true;
+		if(length == true){
+            size = username.length();
+            System.out.println("Username length: " + size);
+        }
+
+		if(username.substring(0).matches("[A-Z][a-z]*")){
+			startLetter = true;
 		}else{
-			rules = false;
+			startLetter = false;
 		}
 
-		if(username.substring().matches("0-9*")){
-			rules = true;
+		if(username.substring().find("0-9*")){
+			haveNumber = true;
 		} else{
-			rules = false;
+			haveNumber = false;
 		}
-		
-		if (rules == true){
+
+		if(username.substring().matches("_") && username.substring().lastIndexOf("_")){
+			underscore = false;
+		} else{
+			underscore = true;
+		}
+
+
+		if (lenght == true && startLetter == true && haveNumber == true && underscore == true){
 			System.out.print("This username complies with the rules");
 		} else{
 			System.out.print("This username is not allowed");
